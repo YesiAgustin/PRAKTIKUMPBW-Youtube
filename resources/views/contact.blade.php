2de793d3be827ceb379feb24be8041837487587e
YesiAgustin <ysiiagstn@gmail.com>
2024-11-21 14:25:48 +0700

    Video2

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    contact
-</body>
-</html>
+<x-app-layout title="contact">
+        Contact
+</x-app-layout>
--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    gallery
-</body>
-</html>
+<x-app-layout title="gallery">
+    Galerry
+</x-app-layout>
