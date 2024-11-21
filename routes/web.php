ef950fbe551d4ff1f2d955d8c08ec1c2f76dbcbf
YesiAgustin <ysiiagstn@gmail.com>
2024-11-21 13:56:38 +0700

    video2

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', fn () => view ('home'));
+Route::get('/about', fn () => view ('about'));
+Route::get('/contact', fn () => view ('contact'));
+Route::get('/gallery', fn () => view ('gallery'));
+
+Route::get ('users', function () {
+ $users = [
+    ['id' => 1, 'name' => 'Yesi' ],
+    ['id' => 2, 'name' => 'Agustin' ],
+ ];
+
+ return $users;
 });
