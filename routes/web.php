927d3249e3038bdfa1515a9227997b93fc1961d0
YesiAgustin <ysiiagstn@gmail.com>
2025-01-02 11:29:13 +0700

    firt commit

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', Controllers\HomeController::class)->name('home');
+
+Route::get('/dashboard', Controllers\HomeController::class)->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [Controllers\ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [Controllers\ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [Controllers\ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
